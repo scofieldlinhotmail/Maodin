7b7edef44414351918b939368eba8e2134b85554
lxc <572362732@qq.com>
2016-01-18 12:27:33 +0800

    center

--- src/js/phone/evaluation.js
+++ src/js/phone/evaluation.js
@@ -15,74 +15,75 @@ var list=[];
 
 
 var $form = $('form');
+$form.each(function(){
+    $(this).validate({
+        debug:true,
+        errorElement: 'span', //default input error message container
+        errorClass: 'help-block', // default input error message class
+        focusInvalid: false, // do not focus the last invalid input
+        ignore: "",
+        rules: {
+            score: {
+                required: true,
+            },
+
+            text: {
+                required:true,
+                minlength: 5
+            },
+        },
+
+        messages: {
+            score: {
+                required: '请选择评分',
+            },
+
+            text: {
+                required: '请填写描述',
+                minlength: "描述最少5个字"
+            },
+
+        },
+
+        invalidHandler: function (event, validator) {
+        },
+
+        highlight: function (element) {
+            $(element)
+                .closest('.am-form-group').addClass('am-form-error');
+        },
+
+        unhighlight: function (element) {
+            $(element)
+                .closest('.am-form-group').removeClass('am-form-error'); // set error class to the control group
+        },
+
+        success: function (label) {
+            label
+                .closest('.am-form-group').removeClass('am-form-error'); // set success class to the control group
+        },
+
+        submitHandler: function (form) {
+            console.log(1);
+            var id=$(form).find(".inputitem").eq(0).val();
+            var s=$(form).find(".inputitem").eq(1).val();
+            var text=$(form).find(".inputitem").eq(2).val();
+            var a=new Object();
+            a.id=id;
+            a.s=s;
+            a.text=text;
+            list.push(a);
+        }
+    });
+})
+
 
 
 $("#sub").click(function(){
     list=[];
     $form.each(function(){
-       $(this).validate({
-           errorElement: 'span', //default input error message container
-           errorClass: 'help-block', // default input error message class
-           focusInvalid: false, // do not focus the last invalid input
-           ignore: "",
-           rules: {
-               score: {
-                   required: true,
-               },
-
-               text: {
-                   required:true,
-                   minlength: 5
-               },
-           },
-
-           messages: {
-               score: {
-                   required: '请填写评分',
-               },
-
-               text: {
-                   required: '请填写描述',
-                   minlength: "描述最少5个字"
-               },
-
-           },
-
-           invalidHandler: function (event, validator) {
-           },
-
-           highlight: function (element) {
-               $(element)
-                   .closest('.am-form-group').addClass('am-form-error');
-           },
-
-           unhighlight: function (element) {
-               $(element)
-                   .closest('.am-form-group').removeClass('am-form-error'); // set error class to the control group
-           },
-
-           success: function (label) {
-               label
-                   .closest('.am-form-group').removeClass('am-form-error'); // set success class to the control group
-           },
-
-           submitHandler: function (form) {
-               var id=$(form).find(".inputitem").eq(0).val();
-               var s=$(form).find(".inputitem").eq(1).val();
-               var text=$(form).find(".inputitem").eq(2).val();
-               var a=new Object();
-               a.id=id;
-               a.s=s;
-               a.text=text;
-               list.push(a);
-           }
-       });
-       $(this).submit();
-
-
-   })
-
-
+        $(this).submit();
+    })
     if(list.length==$('form').length)
     {
         $.each(list,function(i,v){
@@ -104,6 +105,23 @@ $("#sub").click(function(){
         });
         window.location.href="/user/order-list";
     }
-
-
 });
+
+$(".pjbtn").click(function(){
+    $(this).addClass("act");
+    $(this).parent().siblings().find("button").removeClass("act");
+    var data=$(this).attr("data");
+    $(this).parent().parent().next().val(data);
+    //$(this).parents("form").submit();
+
+    $(this)
+        .closest('.am-form-group').removeClass('am-form-error').find("span").html("");
+    
+
+
+
+
+})
+
+
+
