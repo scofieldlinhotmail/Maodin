a3df5615a71638df379876beef1b144dc683cfbd
lxc <572362732@qq.com>
2016-01-22 17:04:35 +0800

    rank

--- routes/admin/Rank.js
+++ routes/admin/Rank.js
@@ -8,9 +8,10 @@ var db = require('../../models/db/index');
 var render = require('../../instances/render.js');
 var debug = require('../../instances/debug.js');
 
+var Rank = db.models.Rank;
+var b=1;
 module.exports = (router) => {
 
-    var Rank = db.models.Rank;
 
     router.get('/adminer-vip/ranks',  function *() {
         var list = yield Rank.findAll();
@@ -26,17 +27,25 @@ module.exports = (router) => {
             return;
         }
         if(this.query.id==0) {
-            yield Rank.create({
-                name: this.query.name,
-                min: this.query.min,
-                max: this.query.max
-            });
+            yield check(this.query.min,this.query.max,this.query.name);
+            if(b==1){
+                yield Rank.create({
+                    name: this.query.name,
+                    min: this.query.min,
+                    max: this.query.max
+                });
+            }
+
         }else{
             var r= yield Rank.findById(this.query.id);
-            r.name=this.query.name;
-            r.min=this.query.min;
-            r.max=this.query.max;
-            yield r.save();
+            yield check(this.query.min,this.query.max,r.name);
+            if(b==1){
+                r.name=this.query.name;
+                r.min=this.query.min;
+                r.max=this.query.max;
+                yield r.save();
+            }
+
         }
         this.body = this.query.title ;
 
@@ -53,6 +62,54 @@ module.exports = (router) => {
         this.body = this.query.id ;
 
     });
+
+    router.get('/adminer-vip/checkname',  function *() {
+        if(this.query.id==0) {
+            var r= yield Rank.findOne({
+                where:{
+                    name:this.query.name
+                }
+            });
+            if(r!=null){
+                this.body = 0;
+            }else{
+                this.body = 1;
+            }
+
+        }else{
+            var r= yield Rank.findOne({
+                where:{
+                    id:{$ne:this.query.id},
+                    name:this.query.name
+                }
+            });
+            if(r!=null){
+                this.body = 0;
+            }else{
+                this.body = 1;
+            }
+        }
+    });
+
+    function *check (min,max,name){
+        min=Number(min);
+        max=Number(max);
+        b=1;
+        var list = yield Rank.findAll();
+        for(var i=0;i<list.length;i++)
+        {
+            if(list[i].min<=min&&list[i].max>=min&&name!=list[i].name){
+                console.log("false"+"--"+"min="+min+"v:"+list[i].name);
+                b=0;
+            }else if(list[i].min<=max&&list[i].max>=max&&name!=list[i].name){
+                console.log("false"+"--"+"max="+max+"v:"+list[i].name);
+                b=0;
+            }
+        }
+
+
+    }
+
 
 
 
--- src/js/admin/rank.js
+++ src/js/admin/rank.js
@@ -2,73 +2,141 @@ require('../admin-base/common.js');
 require('../../css/admin/rank.scss');
 
 var $ = jQuery;
-
+var list=[];
+var edit=0;
+var oldname="";
+var b=1;
+function check(min,max,name){
+    b=1;
+    $.each(list,function(i,v){
+        if(v.min<=min&&v.max>=min&&name!=v.name){
+            //console.log("false"+"--"+"min="+min+"v:"+v.name);
+            b=0;
+        }else if(v.min<=max&&v.max>=max&&name!=v.name){
+            //console.log("false"+"--"+"max="+max+"v:"+v.name);
+            b=0;
+        }
+    })
+}
 $('#add1btn').click(function () {
     var id=  $('#id').val();
     var v = $('#add1').val();
-    var v1 = $('#add2').val();
-    var v2 = $('#add3').val();
+    var v1 = Number($('#add2').val());
+    var v2 = Number($('#add3').val());
+    ///check
+     if(edit==0){
+        // console.log("add");
+     check(v1,v2,v);
+     }
+     else if(edit==1) {
+        // console.log("edit");
+        // console.log("oldname:"+oldname);
+     check(v1,v2,oldname);
+     }
+
+    //console.log(b);
     if (v == "")
         alert("名称不能为空");
     else if(v1>=v2)
         alert("积分上限值必须大于积分下限值")
-    else
+    else if(edit==0&&b==0){
+        alert("与其他等级有积分重复")
+    }
+    else if(edit==1&&b==0) {
+        alert("与其他等级有积分重复")
+    }
+    else{
         $.ajax({
 
-        type: 'Get',
+            type: 'Get',
 
-        url: "ranksave",
+            url: "checkname",
 
-        data: {id:id, name: v, min: v1,max: v2},
+            data: {id:id, name: v},
 
-        success: function (d) {
-           if(id==0){
-               var s='';
-                   s+='<tr class="odd gradeX" id="f'+id+'">'
-                   s+='<td class="n1" style="">'+v+'</td>'
-                   s+='<td class="n2" style="">'+v1+'</td>'
-                   s+='<td class="n3" style="">'+v2+'</td>'
-                   s+='<td>'
-                   s+='<div style="height: 30px" class="btn-group">'
-                   s+='<a href="#addmodal" data="'+id+'" value="'+v+'" data-toggle="modal"'
-                   s+='class="btn default btn-sm edit"><i class="fa fa-edit"></i> 修改</a>'
-                   s+='<a data="'+id+'" class="btn red btn-sm  remove"><i class="fa fa-remove"></i>删除</a>'
-                   s+='</div>'
-                   s+='</td>'
-                   s+='</tr>'
-               $("tbody").append(s);
-               $(".close").click();
-           }else{
-               var $s= $("#f"+id);
-               var s='';
-               s+='<td class="n1" style="">'+v+'</td>'
-               s+='<td class="n2" style="">'+v1+'</td>'
-               s+='<td class="n3" style="">'+v2+'</td>'
-               s+='<td>'
-               s+='<div style="height: 30px" class="btn-group">'
-               s+='<a href="#addmodal" data="'+id+'" value="'+v+'" data-toggle="modal"'
-               s+='class="btn default btn-sm edit"><i class="fa fa-edit"></i> 修改</a>'
-               s+='<a data="'+id+'" class="btn red btn-sm  remove"><i class="fa fa-remove"></i>删除</a>'
-               s+='</div>'
-               s+='</td>'
-               $s.html(s);
-               $(".close").click();
-           }
-        }
+            success: function(ret){
+                if(ret==1){
+                    $.ajax({
+
+                        type: 'Get',
+
+                        url: "ranksave",
+
+                        data: {id:id, name: v, min: v1,max: v2},
+
+                        success: function (d) {
+                            if(id==0){
+                                var s='';
+                                s+='<tr class="odd gradeX" id="f'+id+'">'
+                                s+='<td class="n1" style="">'+v+'</td>'
+                                s+='<td class="n2" style="">'+v1+'</td>'
+                                s+='<td class="n3" style="">'+v2+'</td>'
+                                s+='<td>'
+                                s+='<div style="height: 30px" class="btn-group">'
+                                s+='<a href="#addmodal" data="'+id+'" value="'+v+'" data-toggle="modal"'
+                                s+='class="btn default btn-sm edit"><i class="fa fa-edit"></i> 修改</a>'
+                                s+='<a data="'+id+'" class="btn red btn-sm  remove"><i class="fa fa-remove"></i>删除</a>'
+                                s+='</div>'
+                                s+='</td>'
+                                s+='</tr>'
+                                $("tbody").append(s);
+                                $(".close").click();
+                            }else{
+                                var $s= $("#f"+id);
+                                var s='';
+                                s+='<td class="n1" style="">'+v+'</td>'
+                                s+='<td class="n2" style="">'+v1+'</td>'
+                                s+='<td class="n3" style="">'+v2+'</td>'
+                                s+='<td>'
+                                s+='<div style="height: 30px" class="btn-group">'
+                                s+='<a href="#addmodal" data="'+id+'" value="'+v+'" data-toggle="modal"'
+                                s+='class="btn default btn-sm edit"><i class="fa fa-edit"></i> 修改</a>'
+                                s+='<a data="'+id+'" class="btn red btn-sm  remove"><i class="fa fa-remove"></i>删除</a>'
+                                s+='</div>'
+                                s+='</td>'
+                                $s.html(s);
+                                ///重新绑定
+                                $('.edit').click(function () {
+                                    formclear();
+                                    var id = $(this).attr("data");
+                                    var $p=$(this).parent().parent().parent();
+                                    var name=$p.find(".n1").html();
+                                    var min=$p.find(".n2").html();
+                                    var max=$p.find(".n3").html();
+                                    oldname=name;
+                                    $('#add1').val(name);
+                                    $('#add2').val(min);
+                                    $('#add3').val(max);
+                                    $('#id').val(id);
+                                    edit=1;
+                                });
+                                $(".close").click();
+                            }
+                        }
+                    });
+                }else{
+                    alert("名字有重复,请更改");
+                }
+            }
         });
 
-});
+    }
+    full();
 
+});
 $('.edit').click(function () {
+    formclear();
     var id = $(this).attr("data");
     var $p=$(this).parent().parent().parent();
     var name=$p.find(".n1").html();
     var min=$p.find(".n2").html();
     var max=$p.find(".n3").html();
+    oldname=name;
     $('#add1').val(name);
     $('#add2').val(min);
     $('#add3').val(max);
     $('#id').val(id);
+    edit=1;
 });
 $("#addbtn").click(function(){
     formclear();
@@ -79,10 +147,9 @@ function formclear(){
     $('#add2').val("");
     $('#add3').val("");
     $('#id').val(0);
+    edit=0;
+
 }
-
-
-
 
 $('.remove').click(function (e) {
     var id = $(this).attr('data');
@@ -102,9 +169,30 @@ $('.remove').click(function (e) {
             }
 
     });
-
+    full();
 
 });
+
+
+///填充
+function full(){
+    list=[];
+    var fas=$(".odd");
+    fas.each(function(){
+       var name=$(this).find(".n1").html();
+       var min=$(this).find(".n2").html();
+       var max=$(this).find(".n3").html();
+        var one=new Object();
+        one.min=Number(min);
+        one.max=Number(max);
+        one.name=name;
+        list.push(one);
+    });
+   // console.log(list);
+}
+
+
+full();
 
 
 
